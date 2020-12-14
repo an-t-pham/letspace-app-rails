aa91ea5005fb8e1159e39c31c6ed422533dd0944
anpham148 <anpham148@gmail.com>
2020-12-14 16:50:04 +0000

    establish associations among all models

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,7 @@
 class Property < ApplicationRecord
+    belongs_to :landlord
+    belongs_to :tenant
+    has_many :previous_record
+    has_many :tenants, through: :previous_record
+    has_many :reviews
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,6 @@
 class Tenant < ApplicationRecord
+    has_one :property
+    has_many :previous_record
+    has_many :properties, through: :previous_record
+    has_many :reviews
 end
