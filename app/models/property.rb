42dd3a3cf99dec2bbc00352e8337e20199c5b6d4
anpham148 <anpham148@gmail.com>
2020-12-14 18:07:59 +0000

    create welcome page

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,6 @@ class Property < ApplicationRecord
     belongs_to :landlord
     belongs_to :tenant
     has_many :previous_record
-    has_many :tenants, through: :previous_record
+    has_many :previous_tenants, through: :previous_record, source: :tenant
     has_many :reviews
 end
