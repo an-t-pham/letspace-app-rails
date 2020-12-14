277b4c0cc93e043147b03d7d42907f8f8fa25ea1
anpham148 <anpham148@gmail.com>
2020-12-14 16:05:03 +0000

    create Landlord resource & migration"

---
+++ db/migrate/20201214155427_create_landlords.rb
@@ -0,0 +1,14 @@
+class CreateLandlords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :landlords do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :bio
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
