c91e336aca3f0bd12ec392986a893f50f547663f
anpham148 <anpham148@gmail.com>
2020-12-15 10:36:15 +0000

    created previous record model + migration

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
     belongs_to :landlord
     belongs_to :tenant
-    has_many :previous_record
-    has_many :previous_tenants, through: :previous_record, source: :tenant
+    has_many :previous_records
+    has_many :previous_tenants, through: :previous_records, source: :tenant
     has_many :reviews
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,6 +1,6 @@
 class Tenant < ApplicationRecord
     has_one :property
-    has_many :previous_record
+    has_many :previous_records
     has_many :previous_properties, through: :previous_record, source: :property
     has_many :reviews
 end
