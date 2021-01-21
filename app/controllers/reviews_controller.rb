b65cff3c8280b92550b0a396f98a3e5b790bfdd1
anpham148 <anpham148@gmail.com>
2021-01-21 12:19:42 +0000

    finish front-end design

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,10 @@
 class ReviewsController < ApplicationController
-   before_action :tenant_require_login, only: [:new, :create, :edit, :update, :destroy]
+    before_action :tenant_require_login, only: [:new, :create, :edit, :update, :destroy]
     @@review_id
 
     def new
-          @property = Property.find(params[:property_id])
-          @tenant = Tenant.find(params[:tenant_id])
+          @property = Property.find_by_id(params[:property_id])
+          @tenant = Tenant.find_by_id(params[:tenant_id])
           if tenant_authorized?(@tenant) && authorized_to_review?(@property)
             @review = Review.new(tenant_id: @tenant.id, property_id: @property.id)
             @url = tenant_previous_property_path(@tenant, @property)
@@ -15,8 +15,8 @@ class ReviewsController < ApplicationController
     end
 
     def create
-          @tenant = Tenant.find(params[:tenant_id])
-          @property = Property.find(params[:property_id])
+          @tenant = Tenant.find_by_id(params[:tenant_id])
+          @property = Property.find_by_id(params[:property_id])
           if tenant_authorized?(@tenant) && authorized_to_review?(@property)
              @review = @tenant.reviews.build(review_params)
              @review.property_id = @property.id
@@ -32,46 +32,67 @@ class ReviewsController < ApplicationController
             redirect_to tenant_path(@tenant)
           end
     end
+
+    def show
+        @tenant = Tenant.find_by_id(params[:tenant_id])
+        @property = Property.find_by_id(params[:property_id])
+        @review = Review.find_by_id(params[:id])
+
+        redirect_to tenant_property_path(@tenant, @property)
+    end
     
     def edit
-           @tenant = Tenant.find(params[:tenant_id])
-           @property = Property.find(params[:property_id])
-           @review = Review.find(params[:id])
-          review_exist?(@review)
-          
-          if authorized_to_edit_review?(@review)
-            @@review_id = @review.id
-            @url = tenant_previous_property_path(@tenant, @property)
-          else
-            flash[:error] = "Not authorized to edit this review"
-            redirect_to tenant_path(@tenant)
-          end
+           @tenant = Tenant.find_by_id(params[:tenant_id])
+           @property = Property.find_by_id(params[:property_id])
+           @review = Review.find_by_id(params[:id])
+           if @review
+               if authorized_to_edit_review?(@review)
+                 @@review_id = @review.id
+                 @url = tenant_previous_property_path(@tenant, @property)
+               else
+                 flash[:error] = "Not authorized to edit this review"
+                 redirect_to tenant_path(@tenant)
+               end
+            else
+              flash[:error] = "Review is not found!"
+              landlord_or_tenant_path
+            end
     end
 
 
     def update
-           @tenant = Tenant.find(params[:tenant_id])
-           @property = Property.find(params[:property_id])
-           @review = Review.find(@@review_id)
-           if authorized_to_edit_review?(@review)
-              @review.update(review_params)
-              redirect_to tenant_property_review_path(@tenant, @property, @review)
+           @tenant = Tenant.find_by_id(params[:tenant_id])
+           @property = Property.find_by_id(params[:property_id])
+           @review = Review.find_by_id(@@review_id)
+           if @review
+               if authorized_to_edit_review?(@review)
+                  @review.update(review_params)
+                  redirect_to tenant_property_review_path(@tenant, @property, @review)
+               else
+                  flash[:error] = "Not authorized to edit this review"
+                  redirect_to tenant_path(@tenant)
+               end
            else
-              flash[:error] = "Not authorized to edit this review"
-              redirect_to tenant_path(@tenant)
+               flash[:error] = "Review is not found!"
+               landlord_or_tenant_path
            end
     end
 
     def destroy
-          @tenant = Tenant.find(params[:tenant_id])
-          @property = Property.find(params[:property_id])
-          @review = Review.find(params[:id])
-          if authorized_to_edit_review?(@review)
-             @review.destroy
-             redirect_to tenant_previous_property_path(@tenant, @property)
+          @tenant = Tenant.find_by_id(params[:tenant_id])
+          @property = Property.find_by_id(params[:property_id])
+          @review = Review.find_by_id(params[:id])
+          if @review
+             if authorized_to_edit_review?(@review)
+                @review.destroy
+                redirect_to tenant_previous_property_path(@tenant, @property)
+             else
+               flash[:error] = "Not authorized to delete this review"
+               redirect_to tenant_path(@tenant)
+             end
           else
-            flash[:error] = "Not authorized to delete this review"
-            redirect_to tenant_path(@tenant)
+            flash[:error] = "Review is not found!"
+            landlord_or_tenant_path
           end
     end
 
@@ -79,10 +100,5 @@ class ReviewsController < ApplicationController
         def review_params
             params.require(:review).permit(:rating, :title, :content)
         end
-
-        def review_exist?(review)
-          landlord_or_tenant_path if !review
-        end    
-
-    
+   
 end
